bda1c34da49e8db3769dbadb133dc9a3eacfb818
Gregory <gregoryKot@gmail.com>
2022-06-19 18:21:17 +0300

    milestone2

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function fibonacci (first) {
-
+fibSequence = [0, 1]
+function fibonacci (index) {
+if (index <= 1) {
+    return index
 }
+    for (let i = 0; i < index-2; i++) {
+        fibSequence.push(fibSequence[fibSequence.length-1] + fibSequence[fibSequence.length-2])
+    }
+    return fibSequence[index-1]
+    }
 const index = document.querySelector(".index")
 const number = document.querySelector(".number")
-index.innerHTML = 5;
-number.innerHTML = 6;
+index.innerHTML = 1;
+number.innerHTML= (fibonacci(1))
+
+
