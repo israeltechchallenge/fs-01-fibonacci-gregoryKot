3fb88f76653827b3e3042a2a256e1c62602ff28f
Gregory <gregoryKot@gmail.com>
2022-06-20 19:20:44 +0300

    milestone 3.1

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-fibSequence = [0, 1]
-function fibonacci (index) {
-if (index < 1) {
-    return index
+fibSequence = [0, 1];
+function fibonacci(index) {
+  if (index < 1) 
+    return index;
+  fibSequence.push(
+    fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2]
+  );
+
+  if (index > fibSequence.length) fibonacci(index)
+  
+  return fibSequence[index - 1]; 
 }
-    for (let i = 0; i < index-2; i++) {
-        fibSequence.push(fibSequence[fibSequence.length-1] + fibSequence[fibSequence.length-2])
-    }
-    return fibSequence[index-1]
-    }
-    let index, number;
-    document.querySelector(".btn").addEventListener("click", function() {
-         index = document.querySelector("#index").value;
-    
-         number = document.querySelector(".number").innerHTML = fibonacci(index);
-        })
-    
-    console.log(index, number)
+let index, number;
+document.querySelector(".btn").addEventListener("click", function () {
+  index = document.querySelector("#index").value;
+
+  number = document.querySelector(".number").innerHTML = fibonacci(index);
+});
+
+console.log(index, number);
