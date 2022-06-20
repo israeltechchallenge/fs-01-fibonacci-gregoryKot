f16749edcaf0312328a3ad82f4f86e45c59cf528
Gregory <gregoryKot@gmail.com>
2022-06-20 19:26:28 +0300

    one more commit

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-fibSequence = [0, 1];
-function fibonacci(index) {
-  if (index < 1) 
-    return index;
-  fibSequence.push(
-    fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2]
-  );
-
-  if (index > fibSequence.length) fibonacci(index)
-  
-  return fibSequence[index - 1]; 
-}
-let index, number;
-document.querySelector(".btn").addEventListener("click", function () {
-  index = document.querySelector("#index").value;
-
-  number = document.querySelector(".number").innerHTML = fibonacci(index);
-});
-
-console.log(index, number);
-=======
 fibSequence = [0, 1]
 function fibonacci (index) {
 if (index < 1) {
@@ -40,4 +18,3 @@ if (index < 1) {
     console.log(index, number)
 
 
->>>>>>> 53535379cff20e6242495b2860359b87cb6527ba
