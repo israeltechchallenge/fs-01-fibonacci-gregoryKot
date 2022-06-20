53535379cff20e6242495b2860359b87cb6527ba
Gregory <gregoryKot@gmail.com>
2022-06-20 18:38:24 +0300

    milestone3

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function fibonacci (first) {
-
+fibSequence = [0, 1]
+function fibonacci (index) {
+if (index < 1) {
+    return index
 }
-const index = document.querySelector(".index")
-const number = document.querySelector(".number")
-index.innerHTML = 5;
-number.innerHTML = 6;
+    for (let i = 0; i < index-2; i++) {
+        fibSequence.push(fibSequence[fibSequence.length-1] + fibSequence[fibSequence.length-2])
+    }
+    return fibSequence[index-1]
+    }
+    let index, number;
+    document.querySelector(".btn").addEventListener("click", function() {
+         index = document.querySelector("#index").value;
+    
+         number = document.querySelector(".number").innerHTML = fibonacci(index);
+        })
+    
+    console.log(index, number)
+
+
